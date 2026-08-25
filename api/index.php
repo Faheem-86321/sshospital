7a6993d8a64eec5e60928f6140d95df1dee8a038
Faheem-86321 <user.devfaheem86@gmail.com>
2026-08-25 17:17:48 +0500

    Fix Vercel PHP deployment

--- api/index.php
+++ api/index.php
@@ -1,6 +1,24 @@
-<?php
-// api/index.php
-
-echo "<h1>Hello World! My PHP project is running on Vercel.</h1>";
-echo "<p>Current time: " . date('Y-m-d H:i:s') . "</p>";
+<?php 
+  session_start();
+  ob_start(); 
+if(!isset($_GET['page'])){
+  $_GET['page']= "login";
+}
+if(file_exists("web_temp/". $_GET['page'].".html.php")) 
+{
+  /* Common Header */
+  $head_title = $_GET['page'];
+  include_once("models/logincookie.php");
+  include_once("env/main_config.php");
+  include_once("common/header.php");
+  
+  include_once("web_temp/". $_GET['page'].".html.php");
+  include_once("common/footer.php");
+}else{
+  ?>
+  <img src="images/html-404-error.jpg" width="100%" height="100%">
+  <?php
+  die();
+}
 ?>
+
