c7b19bd577bda1b42b6aa9a4374f08cc7cb47516
Faheem-86321 <user.devfaheem86@gmail.com>
2026-08-25 17:45:38 +0500

    Force append main_config file

---
+++ env/main_config.php
@@ -0,0 +1,22 @@
+<?php
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+$con = mysqli_connect('localhost', 'u719432153_Faheem', '$Bf1Yl=QAYZb', 'u719432153_Faheem');
+
+if (!$con) {
+    die('Connection Not Established');
+}
+
+$company_sql = "SELECT * FROM company_info WHERE status = '1' AND close = '1'";
+$company_sql_ex = mysqli_query($con, $company_sql);
+
+while ($key = mysqli_fetch_assoc($company_sql_ex)) {
+    $_SESSION['com_name']    = $key['com_name'];
+    $_SESSION['com_phone']   = $key['com_phone'];
+    $_SESSION['com_email']   = $key['com_email'];
+    $_SESSION['com_logo']    = $key['com_logo'];
+    $_SESSION['com_address'] = $key['com_address'];
+}
+?>
